9d597a3ba0dedd4ca71267868b381ed606017b7a
Peter Major <peter.major@forgerock.com>
2017-01-25 01:21:34 +0000

    OPENIDM-6709 Inactive users can still Reset Password through Self Service

--- self-service/forgerock-selfservice-stages/src/main/java/org/forgerock/selfservice/stages/user/UserQueryStage.java
+++ self-service/forgerock-selfservice-stages/src/main/java/org/forgerock/selfservice/stages/user/UserQueryStage.java
@@ -141,6 +141,10 @@ public final class UserQueryStage implements ProgressStage<UserQueryConfig> {
             throw new BadRequestException("Unable to find account");
         }
 
+        if (user.get(0).get("accountStatus").asString().equals("inactive")) {
+            throw new BadRequestException("Account is inactive");
+        }
+
         return user.get(0);
     }
 
