23597a6387b45d0464d5a2da58d8440c5ff6c88a
sean oneill <sean.oneill@forgerock.com>
2017-02-20 11:50:14 +0100

    COMMONS-142 updating der encoding to align with latest JDK constraints

--- json-web-token/src/main/java/org/forgerock/json/jose/jws/handlers/ECDSASigningHandler.java
+++ json-web-token/src/main/java/org/forgerock/json/jose/jws/handlers/ECDSASigningHandler.java
@@ -157,11 +157,14 @@ public class ECDSASigningHandler implements SigningHandler {
 
         final int size = params.position();
         // The overall sequence may need up to 4 bytes for the length field plus 1 byte for the sequence tag.
-        final ByteBuffer sequence = ByteBuffer.allocate(size + 5);
+        final ByteBuffer sequence = ByteBuffer.allocate(size + 6);
         sequence.put(DerUtils.SEQUENCE_TAG);
         DerUtils.writeLength(sequence, size);
         sequence.put((ByteBuffer) params.flip());
-        return sequence.array();
+        sequence.flip();
+        final byte[] encodedSignature = new byte[sequence.remaining()];
+        sequence.get(encodedSignature);
+        return encodedSignature;
     }
 
 }
